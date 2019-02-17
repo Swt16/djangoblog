01c3464de1af2dc42f9cefaa4f8e009267452bdc
Shin <swtran21@gmail.com>
2019-02-17 09:47:45 -0800

    Lesson 7 Assignment Part 1
    Reuploading for a better commit description.

--- mysite/myblog/admin.py
+++ mysite/myblog/admin.py
@@ -1,4 +1,6 @@
-from django.contrib import admin  # <- this is already there.
+from django.contrib import admin
+from myblog.models import Category
 from myblog.models import Post
 
 admin.site.register(Post)
+admin.site.register(Category)
