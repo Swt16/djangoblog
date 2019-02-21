d94c06dddac26c7d98d87e9c611b33143b19d421
Shin <swtran21@gmail.com>
2019-02-20 19:39:22 -0800

    Lesson 7 Assignment Part 2

--- mysite/myblog/admin.py
+++ mysite/myblog/admin.py
@@ -2,5 +2,17 @@ from django.contrib import admin
 from myblog.models import Category
 from myblog.models import Post
 
-admin.site.register(Post)
-admin.site.register(Category)
+
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CategoryInline,]
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [CategoryInline,]
+    exclude = ('posts',)
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
